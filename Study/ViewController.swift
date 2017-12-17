e5200013b9f211f78b526077ab34d4747020d888
James <jamezbrown100@gmail.com>
2017-12-17 11:26:47 -0600

    Initial Commit

---
+++ Study/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Study
+//
+//  Created by James Brown on 12/17/17.
+//  Copyright © 2017 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
